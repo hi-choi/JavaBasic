70c84d2d462809ae31a46a82a1281caffc1b7877
hi-choi <gkssk3577@naver.com>
2021-04-21 17:48:31 +0900

    JDBC 프로그래밍 기초

--- src/hi_choi/basic/day16/HelloJDBC.java
+++ src/hi_choi/basic/day16/HelloJDBC.java
@@ -23,7 +23,7 @@ public class HelloJDBC {
 
         // amazon 데이터베이스 접속 확인
         String drv = "org.mariadb.jdbc.Driver";
-        String amz = "";
+        String amz = ".com";
         String url = "jdbc:mariadb://"+amz+":3306/bigdata";
         String usr = "bigdata";
         String pwd = "bigdata2020";
